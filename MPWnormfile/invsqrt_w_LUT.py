a804c8e94fe73a8ddb8551bca8190a4d8b987269
Jihyeon Heo <144996522+heo-ji@users.noreply.github.com>
2024-08-03 23:59:01 +0900

    Create invsqrt_w_LUT.py

---
+++ MPWnormfile/invsqrt_w_LUT.py
@@ -0,0 +1,14 @@
+'''
+value_name(Qformat) = example value
+--------------------------------
+accumulate value(18.8) = 3FFCF4E
+accumulate squared value (26.8) = 000004EC45
+
+mean(8.8) = FFEF
+mean_by squared value (16.16) = 0001A25C
+
+variance(8.16) = 01A13B
+inverse square root of variance (8.8) = 00C7
+
+'''
+
